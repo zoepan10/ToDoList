817e1f60917e217cd2ef58d84adb41f3f0ea3e4e
Zoe Pan <zoe.yi.pan@gmail.com>
2025-08-08 15:24:15 -0700

    Added delete funtion to let users delete tasks from the todo list

--- ToDoList/ContentView.swift
+++ ToDoList/ContentView.swift
@@ -11,6 +11,7 @@ import SwiftData
 struct ContentView: View {
     @State private var showNewTask = false
     @Query var toDos: [ToDoItem]
+    @Environment(\.modelContext) var modelContext
     var body: some View {
         VStack {
             HStack {
@@ -41,11 +42,19 @@ struct ContentView: View {
                         Text(toDoItem.title)
                     }
                 }
+                .onDelete(perform: deleteToDo)
             }
             .listStyle(.plain)
         }
         if showNewTask {
             NewToDoView(showNewTask: $showNewTask, toDoItem: ToDoItem(title: "", isImportant: false))
+        }
+    }
+    
+    func deleteToDo(at offsets: IndexSet) {
+        for offset in offsets {
+            let toDoItem = toDos[offset]
+            modelContext.delete(toDoItem)
         }
     }
 }
